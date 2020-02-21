ce90c389b80421aa2d68a1fa0a23fa6bf4f17d0e
Malcom Fetus <sora.vista@gmail.com>
2020-02-21 16:10:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+katz_deli = []
+def line(array)
+ if array.length == 0
+   puts "The line is currently empty."
+
+else
+  message = "The line is currently:"
+   array.each_with_index do |value, index|
+    message += " #{index.to_i+1}. #{value}"
+end
+puts "#{message}"
+ end
+ end
+
+def take_a_number(katz_deli)
+katz_deli = []
+if katz_deli.join
+  binding.pry
+
+  message = "Welcome"
+  katz_deli.each_with_index do |value, index|
+  message += "#{katz_deli}. You are number #{index.to_i+1} in line."
+
+end
+puts "#{message}"
+end
+end
