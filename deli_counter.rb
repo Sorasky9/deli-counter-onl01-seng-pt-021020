f1f77006c57b19eaef26a04b9aaa85953cb181b4
Malcom Fetus <sora.vista@gmail.com>
2020-02-21 16:25:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,13 +13,16 @@ puts "#{message}"
  end
  end
 
-def take_a_number(array, name)
-array.push("Ada")
-if array.push = "Ada"
-  message = "Welcome"
-  katz_deli.each_with_index do |value, index|
-  message += "#{katz_deli}. You are number #{index.to_i+1} in line."
-end
-puts "#{message}"
-end
-end
+ def take_a_number(array, name)
+   array.push(name)
+   position = array.index(name)
+   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+   return name, position
+ end
+ def now_serving(array)
+   if array.empty? == true
+     puts "There is nobody waiting to be served!"
+   elsif array.empty? == false
+     puts "Currently serving #{array.shift}."
+   end
+ end 
