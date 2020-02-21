8fb64dbd850759cc1f0439d9f02f44e6c80185ef
Malcom Fetus <sora.vista@gmail.com>
2020-02-21 16:16:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,8 @@ puts "#{message}"
  end
  end
 
+ katz_deli = ["Ada"]
 def take_a_number(katz_deli)
-katz_deli = []
-
 if katz_deli.join
   message = "Welcome"
   katz_deli.each_with_index do |value, index|
